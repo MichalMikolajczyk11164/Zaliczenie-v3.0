b87a7317114929e219438b7084113cb074646494
AdamOsmanski <adamosmanski1@gmail.com>
2020-05-13 01:36:25 +0200

    Update Game.cpp
    Poprawienie błędów, oraz nadanie czasu.

--- Zaliczenie v3.0/Game.cpp
+++ Zaliczenie v3.0/Game.cpp
@@ -1,30 +1,74 @@
-#include "Game.h"w
+#include "Game.h"
 
-
+/**
+Inicjalizacja okna.
+*/
 void Game::initWindow()
 {
+	this->window = new RenderWindow(VideoMode(800, 600), "RPG");
 }
-
+/**
+	Konstruktor i destruktor
+*/
 Game::Game()
 {
+	this->initWindow();
 }
 
 Game::~Game()
 {
+	delete this->window;
 }
 
+void Game::updateTime()
+{
+	this->time = timeClock.restart().asSeconds();
+
+	system("cls");
+	cout << time << "\n";
+}
+
+/**
+	Ta funkcja odpowiada za renderowanie przedmiotów.
+*/
 void Game::render()
 {
+	this->window->clear();
+	this->window->display();
 }
 
 void Game::update()
 {
+	this->updateEvents();
 }
 
+/**
+	Uruchomienie okna.
+*/
 void Game::run()
 {
+	while (this->window->isOpen())
+	{
+		this->updateTime();
+		this->update();
+		this->render();
+	}
 }
 
 void Game::updateEvents()
 {
+
+	while (this->window->pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case Event::Closed:
+			this->window->close();
+			break;
+		case Event::KeyPressed:
+			if (event.key.code == Keyboard::Escape)
+				this->window->close();
+			break;
+		}
+	}
 }
